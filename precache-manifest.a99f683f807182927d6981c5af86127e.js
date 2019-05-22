d6a908a5371cf94140929383ea004f3a961bb627
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 22:09:04 +0000

    Deploy MananS77/grocery-helper-ui to github.com/MananS77/grocery-helper-ui.git:gh-pages

---
+++ precache-manifest.a99f683f807182927d6981c5af86127e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ba2cde51f81f94d0cab8c09cb87bed",
+    "url": "/MananS77/grocery-helper-ui/index.html"
+  },
+  {
+    "revision": "5554d68b26b764fc2f12",
+    "url": "/MananS77/grocery-helper-ui/static/css/main.9c6a4266.chunk.css"
+  },
+  {
+    "revision": "a298d6b4b3abeba3c2a1",
+    "url": "/MananS77/grocery-helper-ui/static/js/2.9b423dc3.chunk.js"
+  },
+  {
+    "revision": "5554d68b26b764fc2f12",
+    "url": "/MananS77/grocery-helper-ui/static/js/main.bbe345fd.chunk.js"
+  },
+  {
+    "revision": "7bfb56e977fdffe59813",
+    "url": "/MananS77/grocery-helper-ui/static/js/runtime~main.d80976b8.js"
+  }
+]);
